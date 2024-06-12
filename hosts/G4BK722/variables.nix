268097007d2d43a50cb4cafd98776eb48ea5654f
princedimond <162772652+princedimond@users.noreply.github.com>
2024-06-12 12:57:08 -0400

    Add files via upload

---
+++ hosts/G4BK722/variables.nix
@@ -0,0 +1,20 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "princedimond";
+  gitEmail = "princedimond@gmail.com";
+
+  # Base16 Theme
+  theme = "dracula";
+
+  # Hyprland Settings
+  borderAnim = true; # Enable / Disable Hyprland Border Animation
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+  waybarAnimations = false;
+
+  # Program Options
+  browser = "microsoft-edge"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+}
